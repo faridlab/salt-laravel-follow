82a9652b5f37e7767a30b4aa96f3e3b4677b7db0
Farid Hidayat <e.faridhidayat@gmail.com>
2022-10-11 23:37:14 +0700

    migrate: follows migration

---
+++ database/migrations/2022_10_11_222943_create_follows_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('follows', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+
+            $table->string('foreign_table')->nullable();
+            $table->string('foreign_id')->nullable();
+
+            $table->string('scope')->nullable(); // eg: user, channel
+
+            $table->foreignUuid('created_by')->references('id')->on('users');
+            $table->foreignUuid('updated_by')->nullable()->references('id')->on('users');
+            $table->foreignUuid('deleted_by')->nullable()->references('id')->on('users');
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('follows');
+    }
+};
